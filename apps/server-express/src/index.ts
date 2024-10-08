02c3d8c9fc1792ffd68237ba5c92128a61a9f001
Ivan Tanev <vankata.t@gmail.com>
2024-10-08 19:02:51 +0300

    pnpm workspaces and example apps

---
+++ apps/server-express/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+function createServer(): express.Express {
+  const app = express();
+  app.get("/", (_, res) => {
+    return res.json({ status: "running" });
+  });
+
+  return app;
+}
+
+function main() {
+  const port = process.env.PORT || 5001;
+  const server = createServer();
+
+  server.listen(port, () => {
+    console.log(`server-express is running on http://localhost:${port}`);
+  });
+}
+
+main();
